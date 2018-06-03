45399dd6483e6b7735070e1f27cef196e4a595d4
Jesse Aguirre <jesse@jesses-mbp.attlocal.net>
2018-06-03 00:28:55 -0700

    chatroom css

--- src/components/WhosOnlineList/WhosOnlineList.jsx
+++ src/components/WhosOnlineList/WhosOnlineList.jsx
@@ -7,8 +7,9 @@ class WhosOnlineList extends Component {
             <ul>
                 {this.props.membersInChatroom.length ?
                     this.props.membersInChatroom.map((member, idx) => <WhosOnlineListItem key={idx} member={member} user={this.props.user}/>)
+                    
                 :
-                <p>Loading...</p>
+                <p className='loading'>Loading...</p>
                 }
             </ul>
         );
@@ -18,32 +19,30 @@ class WhosOnlineList extends Component {
 const WhosOnlineListItem = (props) => {
     const styles = {
         li: {
-            display: 'flex',
+            display: 'table-row',
             alignItems: 'center',
-            marginTop: 5,
-            marginBottom: 5,
+            marginTop: 50,
+            marginBottom: 50,
             paddingTop: 2,
             paddingBottom: 2,
         },
         div: {
             borderRadius: '50%',
             width: 11,
-            height: 11,
             marginRight: 10,
         },
     };
-
     
     return props.member._id === props.user._id ?
         <li style={styles.li}>
             <div style={styles.div}>
-                <p className='userName'><span>{props.member.firstName} {props.member.lastName}</span> {props.member.email}</p>(you)
+                <p className='userName'><span className='fullName'>> {props.member.firstName} {props.member.lastName}</span>&nbsp;{props.member.email}</p>
             </div>
         </li>
         :
         <li style={styles.li}>
         <div style={styles.div}>
-            <p className='userName'><span>{props.member.firstName} {props.member.lastName}</span> {props.member.email}</p>
+            <p className='otherUsers'><span className='fullName'>> {props.member.firstName} {props.member.lastName}</span>&nbsp;<a className='email' href={`mailto:${props.member.email}`}>{props.member.email}</a></p>
         </div>
         </li>;
 
--- src/pages/ChatRoomPage/ChatRoomPage.jsx
+++ src/pages/ChatRoomPage/ChatRoomPage.jsx
@@ -30,7 +30,7 @@ class ChatRoomPage extends Component {
     handleSendChat = () => {
         var content = this.chatInput.value;
         if (!this.chatInput) return;
-        socket.emit('new-chat', { content, userId: this.props.user._id, userName: this.props.user.firstName });
+        socket.emit('new-chat', { content, userId: this.props.user._id, userName: `${this.props.user.firstName} ${this.props.user.lastName}`});
         this.chatInput.value = "";
         this.forceUpdate();
     }
@@ -55,28 +55,25 @@ class ChatRoomPage extends Component {
                 <div>
                     <NavBar />
                     <div className='container-chat-rooms' style={{ backgroundColor: 'white' }}>
-                        <div className='chatContainer'>
+                        <div className='chatContainer'>                         
                             <aside className='whosOnlineListContainer'>
                                 <h1 className='chatRoomTitle'>{this.state.topic.title} Chatroom</h1>
+                                <p className='currentUsers'>Current Users: <span className='badge badge-light'>{this.state.membersInChatroom.length}</span></p>
                                 <hr />
                                 <WhosOnlineList
                                     membersInChatroom={this.state.membersInChatroom}
                                     user={this.props.user}
                                 />
-                            </aside>
+                            </aside>                            
                             <section className='chatListContainer'>
                                 <div className='chats'>
-                                    {this.state.topic.chats.map((chat, idx) => <div key={idx}><hr /><span>{chat.content}</span></div>)}
+                                    {this.state.topic.chats.map((chat, idx) => <div className='chatBox' key={idx}><p className='chatName'>{chat.userName} <span className='datetime'>{new Date(chat.createdAt).toLocaleString().replace(/:\d\d /, ' ',).replace(',', ' -')}</span></p><hr /><p className='chatContent'>{chat.content}</p></div>)}
                                 </div>
-                                {/* <div className='chatListContainer'>
-                                    <textarea ref={(ta) => this.chatInput = ta}></textarea>
-                                    <button className='btn btn-outline-secondary' onClick={this.handleSendChat}><i className="fas fa-paper-plane"></i></button> */}
-
-                                    <div className="input-group">
-                                        <textarea ref={(ta) => this.chatInput = ta} className="form-control custom-control" rows="3" ></textarea>
-                                        <span className="input-group-addon btn btn-outline-secondary" onClick={this.handleSendChat}><i className="fas fa-paper-plane"></i></span>
-                                    </div>
-                                {/* </div> */}
+                                <div className="input-group">
+                                    <textarea ref={(ta) => this.chatInput = ta} className="form-control custom-control chat-input" rows="3" placeholder='Type here...'></textarea>
+                                    <span className="input-group-addon btn btn-dark" onClick={this.handleSendChat}><i className="fas fa-paper-plane"></i></span>
+                                </div>
+                                
                             </section>
                         </div>
                     </div>
